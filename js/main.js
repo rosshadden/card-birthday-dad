1596c946a9209fdc15fa818570a7fd4aefe5d256
Ross Hadden <rosshadden@gmail.com>
2013-04-03 10:53:48 -0400

    Done! maybe.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,23 @@
 $(function(){
-	$('#content')
-		.children('#cake')
-			.draggable();
+	$('#envelope')
+		.on('click', '#envelope_front', function(){
+			$('#envelope_front').toggleClass('flipped');
+			$('#envelope_back').toggleClass('flipped');
+		})
+		.on('click', '#flap_outside', function(){
+			$('#flap_outside').toggleClass('open');
+			$('#flap_inside').toggleClass('open');
 
-	// Add Flip Envelope Event Handler
-	document.getElementById('envelope_front').onclick = function(){
-		document.getElementById('envelope_front').classList.toggle('flipped');
-		document.getElementById('envelope_back').classList.toggle('flipped');
-	}
+			$('#envelope_back_outside').on('click', function(){
+				$('#card').toggleClass('removed');
+				return false;
+			});
 
-	// Add Open Envelope Event Handler
-	document.getElementById('flap_outside').onclick = function(){
-
-		document.getElementById('flap_outside').classList.toggle('open');
-		document.getElementById('flap_inside').classList.toggle('open');
-
-		// Add Remove Card Event Handler
-		// This is added after "Open Envelope" so that card can't be removed
-		// until the envelope has been opened
-		document.getElementById('envelope_back_outside').onclick = function(){
-			document.getElementById('card').classList.toggle('removed');
 			return false;
-		}
-
-		return false;
-	}
-
-	// Open Card
-	document.getElementById('card').onclick = function(){
-		document.getElementById('card_outside_front').classList.toggle('open');
-		document.getElementById('card_inside_top').classList.toggle('open');
-		return false;
-	}
+		})
+		.on('click', '#card', function(){
+			$('#card_outside_front').toggleClass('open');
+			$('#card_inside_top').toggleClass('open');
+			return false;
+		});
 });
